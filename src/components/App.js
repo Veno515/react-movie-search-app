3012019291fcce5309dc4e8807d799440b099799
Veno515 <veinoyusuf@gmail.com>
2019-12-10 13:04:25 +0100

    API Key modified

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import Movie from "./Movie";
 import Search from "./Search";
 
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=7e636df9
+";
 
 
 const initialState = {
